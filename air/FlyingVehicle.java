0f7324fb1ad0ddf10ad18808754e13a3772d7f1c
Sahar Ramezani (Alba) <122964803+SaharRamezani@users.noreply.github.com>
2025-10-05 12:49:15 +0200

    Add files via upload

---
+++ air/FlyingVehicle.java
@@ -0,0 +1,17 @@
+public class FlyingVehicle extends Vehicle {
+    String fuel;
+    int numberOfFins;
+    int numberOfWheels;
+    String steeringWheel;
+    
+    public FlyingVehicle(String name, int price, 
+                        int numberOfSeats, int maxSpeed, 
+                        String fuel, int numberOfFins, 
+                        int numberOfWheels, String steeringWheel) {
+        super(name, price, numberOfSeats, maxSpeed);
+        this.fuel = fuel;
+        this.numberOfFins = numberOfFins;
+        this.numberOfWheels = numberOfWheels;
+        this.steeringWheel = steeringWheel;
+    }
+}
---
+++ air/Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+    String name;
+    int price;
+    int numberOfSeats;
+    int maxSpeed;
+
+    public Vehicle(String name, int price, int numberOfSeats, int maxSpeed) {
+        this.name = name;
+        this.price = price;
+        this.numberOfSeats = numberOfSeats;
+        this.maxSpeed = maxSpeed;
+    }
+}
